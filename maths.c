b9fb0a11b06531f4572eb3a64d12601150f4b85b
Christopher Powell <p14160913@myemail.dmu.ac.uk>
2016-01-15 13:45:39 +0000

    Created Subtract Function

--- maths.c
+++ maths.c
@@ -6,10 +6,19 @@ int add(int x, int y)
 	return x + y; 
 }
 
+int subtract(int x, int y)
+{
+	if(x >= y)
+		return x - y;
+	else
+		return y - x;
+}
+
 int main()
 {
 	int x = 4;
 	int y = 20;
 	
 	printf("Add Function = %i\n", add(x, y));
+	printf("Subtract Function = %i\n", subtract(x, y));
 }
